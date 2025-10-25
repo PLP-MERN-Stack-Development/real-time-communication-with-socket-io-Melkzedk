ffdae8f36082479f71714740c64523023c3e3a13
Melkzedk <melkywafula29@gmail.com>
2025-10-25 18:52:29 +0300

    updated socket.js file

---
+++ client/src/socket/socket.js
@@ -0,0 +1,9 @@
+import { io } from 'socket.io-client';
+// createSocket receives token or query params (userId/username) for demo
+simplicity
+export function createSocket({ userId, username }) {
+const socket = io(process.env.REACT_APP_API_URL || 'http://localhost:5000', {
+query: { userId, username }
+});
+return socket;
+}
