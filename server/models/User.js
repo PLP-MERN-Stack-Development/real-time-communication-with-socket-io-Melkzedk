75c9691d605da0296a59d15d9dbe09c94f721e00
Melkzedk <melkywafula29@gmail.com>
2025-10-26 11:01:30 +0300

    changes

--- server/models/User.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const UserSchema = new mongoose.Schema({
-username: { type: String, required: true, unique: true },
-password: { type: String }, // optional if using username-only auth
-online: { type: Boolean, default: false },
-socketId: { type: String, default: null }
-}, { timestamps: true });
-module.exports = mongoose.model('User', UserSchema);
